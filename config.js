1d4a315a2ef23cc278621eb54d2d0f146367d7cf
ayoser2 <107592051+ayoser2@users.noreply.github.com>
2022-07-08 06:48:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -46,18 +46,18 @@ global.ucpn = ''
 global.linkgc = 'https://chat.whatsapp.com/HZ4c5yrsd0g8OfELZGySFZ'
 global.linkig = 'https://www.instagram.com/neer_j_  '
 global.linkyt = 'https://www.youtube.com/channel/UCLegt7MKqNBxJjIkE_QNPdA'
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.git = 'https://github.com/Neeraj-x0'
-global.web1 = 'https://neeraj-x0.github.io/'
+global.linkfb = 'http://wa.me/94772496127'
+global.git = '𝚝𝚑𝚒𝚜 𝚙𝚛𝚘𝚓𝚎𝚌𝚝 𝚒𝚜 𝚋𝚎𝚒𝚗𝚐 𝚝𝚎𝚜𝚝'
+global.web1 = 'https://.github.io/'
 global.url = 'https://chat.whatsapp.com/HZ4c5yrsd0g8OfELZGySFZ'
 
 //HEROKU  VARS
 global.thumburl = process.env.IMAGE || 'https://i.pinimg.com/474x/de/bd/b2/debdb24645169bf95eecd49f3144315f.jpg'
-global.packname = process.env.PACKNAME ||'ᴍɪʟʟɪᴇ-ᴍᴅ'
-global.author = process.env.AUTHOR ||'ɴᴇᴇʀᴀᴊ-x0'
-global.name = 'Millie MULTI-DEVICE'
+global.packname = process.env.PACKNAME ||'𝙱𝚢'
+global.author = process.env.AUTHOR ||'𝙰 𝚈 𝙾'
+global.name = '𝙂𝙖𝙧𝙛𝙞𝙚𝙡𝙙-𝙈𝘿'
 global.session = process.env.SESSION_ID|| ''
-global.wm = process.env.WATERMARK|| 'ᴍɪʟʟɪᴇ-ᴍᴅ'
+global.wm = process.env.WATERMARK|| '𝙜𝙖𝙧𝙛𝙞𝙚𝙡𝙙 𝙈𝘿'
 global.owner_name = process.env.OWNER_NAME || 'Neeraj-x0'
 global.logsmsg = convertToBool(process.env.LOGS)|| false
 
@@ -71,15 +71,15 @@ function convertToBool(text, fault = 'true') {
 
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = 'neer_j_'
+global.bodlink = '𝙰 𝚈 𝙾'
 global.wait = '_Please Wait_'
 global.eror = '_Something went wrong please try again later_'
-global.dtu = 'BOT HELP GROUP'
+global.dtu = 'BOT GROUP'
  
 
 
 
-global.version = '2.0.1(ʙᴇᴛᴀ)'
+global.version = '4.0.0 𝚅'
 
 global.multiplier = 69 // The higher, The harder levelup
 global.nolink = 'No URL Found'
